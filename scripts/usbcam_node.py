724238b04bc856f5731afa7f5815472d50009a76
Juxi <juxi.leitner@gmail.com>
2015-07-28 17:50:21 +1000

    cropping image in usbcam node
    usb cam node is now cropping the image (also unnecessary dependencies
    have been removed)

--- scripts/usbcam_node.py
+++ scripts/usbcam_node.py
@@ -4,13 +4,9 @@
 
 import argparse
 import rospy
-import baxter_interface
-import baxter_external_devices
 import cv2
 import cv_bridge
 
-from baxter_interface import CHECK_VERSION
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
 class CameraNode(object):
@@ -20,6 +16,7 @@ class CameraNode(object):
     def __init__(self, device=0):
         self.is_stopping = False
         self.rate = rospy.Rate(20) # Hz
+        self.crop_width, self.crop_height = 86, 86
 
         self.bridge = cv_bridge.CvBridge()
         self.cam = cv2.VideoCapture(device)
@@ -41,13 +38,24 @@ class CameraNode(object):
 
             # TODO do some operation on the camera image here!
 
+            # Crop from x, y, w, h -> 100, 200, 300, 400
+            # img[200:400, 100:300]
+
+            #height, width, channels = img.shape
+            #middle_h, middle_w = height/2, width/2
+            x, y = 100, 100
+            cropped_frame = frame[x:x+self.crop_width,
+                                  y:y+self.crop_height]
+            #cropped_frame = frame[middle_h-crop_width/2:middle_w-crop_height/2,
+
             # convert frame and publish message
             # msg = self.bridge.cv2_to_imgmsg(frame, encoding="passthrough")
-            msg = self.bridge.cv2_to_imgmsg(frame, "mono8")
+            msg = self.bridge.cv2_to_imgmsg(cropped_frame, "mono8")
             self.pub.publish(msg)
 
             # sleep to create a control frequency
             self.rate.sleep()
+
         self.cam.release()
 
 
